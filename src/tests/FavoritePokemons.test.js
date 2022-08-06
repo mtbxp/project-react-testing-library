7927800e8f578bec01bd42a08f63698c249b0c78
mtbxp <marcellotb2@gmail.com>
2022-08-06 12:42:23 -0300

    3o Requisito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+test('se é exibida na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const noFav = screen.getByText(/no favorite pokemon found/i);
+  expect(noFav).toBeInTheDocument();
+});
