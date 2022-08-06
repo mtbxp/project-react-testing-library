1d1ab20693c1f337df5defc381fb7c7b988548d6
mtbxp <marcellotb2@gmail.com>
2022-08-06 11:33:14 -0300

    1o Requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+  renderWithRouter(<App />);
+
+  const home = screen.getByRole('link', { name: /home/i });
+  expect(home).toBeInTheDocument();
+
+  const about = screen.getByRole('link', { name: /about/i });
+  expect(about).toBeInTheDocument();
+
+  const favorite = screen.getByRole('link', { name: /favorite pokémons/i });
+  expect(favorite).toBeInTheDocument();
+});
+
+test('se redireciona para a pagina Not Found quando não for uma URL válida', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('./qualquercoisa');
+
+  const notFound = screen.getByText(/not found/i);
+  expect(notFound).toBeInTheDocument();
+});
