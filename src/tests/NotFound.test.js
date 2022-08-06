58e82e004f5f78f5febce05a9911d0d54bbd358b
mtbxp <marcellotb2@gmail.com>
2022-08-06 12:59:07 -0300

    4o Requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+test('se a página contém um heading h2 com o texto Page requested not found;', () => {
+  renderWithRouter(<NotFound />);
+  const notFound = screen.getByRole('heading', {
+    name: /page requested not found crying emoji/i, level: 2 });
+  expect(notFound).toBeInTheDocument();
+});
+
+test('se a página contém a seguinte imagem de uma Pokédex', () => {
+  renderWithRouter(<NotFound />);
+  const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const img = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i });
+  expect(img).toHaveAttribute('src', url);
+});
